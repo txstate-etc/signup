58410ffe0c42d0183fb08c7bc49ec463701abaee
Sean McMains <seanmctex@txstate.edu>
2010-06-22 15:36:43 -0500

    Oops, forgot to update unit tests.

--- test/unit/reservation_test.rb
+++ test/unit/reservation_test.rb
@@ -1,34 +1,34 @@
 require 'test_helper'
 
 class ReservationTest < ActiveSupport::TestCase
-  fixtures :topics, :reservations, :admins
+  fixtures :topics, :reservations, :users
   
   test "Shouldn't be able to make reservations for sessions in the past" do
-    reservation = Reservation.new( :session => sessions( :gato_past ), :login => "testuser", :name => "Test User" )
+    reservation = Reservation.new( :session => sessions( :gato_past ), :user => users( :plainuser1 ) )
     assert !reservation.save
   end
   
   test "Shouldn't be able to make reservations for sessions that have been cancelled" do
-    reservation = Reservation.new( :session => sessions( :gato_cancelled ), :login => "testuser", :name => "Test User" )
+    reservation = Reservation.new( :session => sessions( :gato_cancelled ),  :user => users( :plainuser1 ) )
     assert !reservation.save
   end
   
   test "Shouldn't be able to make a reservation if the class is already filled up" do
-    reservation = Reservation.new( :session => sessions( :tracs_tiny ), :login => "testuser", :name => "Test User" )
+    reservation = Reservation.new( :session => sessions( :tracs_tiny ),  :user => users( :plainuser1 ) )
     assert reservation.save
-    reservation = Reservation.new( :session => sessions( :tracs_tiny ), :login => "otheruser", :name => "Other User" )
+    reservation = Reservation.new( :session => sessions( :tracs_tiny ),  :user => users( :plainuser2 ) )
     assert !reservation.save    
   end
   
   test "The same person shouldn't be able to register for a class more than once" do
-    reservation = Reservation.new( :session => sessions( :gato ), :login => "testuser", :name => "Test User" )
-    assert reservation.save
-    reservation = Reservation.new( :session => sessions( :gato ), :login => "testuser", :name => "Test User" )
+    reservation = Reservation.new( :session => sessions( :gato ),  :user => users( :plainuser2 ) )
+    assert reservation.save, reservation.errors.to_s
+    reservation = Reservation.new( :session => sessions( :gato ),  :user => users( :plainuser2 ) )
     assert !reservation.save    
   end
   
   test "Should be able to make a reservation for an otherwise valid class" do 
-    reservation = Reservation.new( :session => sessions( :gato ), :login => "testuser", :name => "Test User" )
-    assert reservation.save
+    reservation = Reservation.new( :session => sessions( :gato ),  :user => users( :plainuser2 ) )
+    assert reservation.save, reservation.errors.to_s
   end
 end
