93555a930afba7ac0d9a0e74d4d781a89660704c
Charles Jones <cj32@txstate.edu>
2010-11-15 18:27:28 -0600

    typo. bugid: 4012

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -69,7 +69,6 @@ class SessionsController < ApplicationController
   def attendance
     @session = Session.find( params[ :id ] )
     @items = @session.reservations.select{|i| i.confirmed? }.sort { |a,b| a.user.name <=> b.user.name }
-    @items.reject!
     @items_per_page = 12
     @page_count = (@items.length + @items_per_page - 1) / @items_per_page
     @page_title = @session.time.to_s + ": " + @session.topic.name
