1e657510908ddbdc32c93ba182ee8b9da427d0a3
Sean McMains <seanmctex@txstate.edu>
2010-11-16 15:23:01 -0600

    Added unit tests, fixed a bug for instructor searching.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.name_like_all_or_login_like_all( params[:search].split(/\s+/) )
+    @users = User.name_like_all_or_login_like_all( params[ :search ].split(/\s+/) ) if !params[ :search ].blank?
   end
 
 end
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -3,7 +3,12 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
   # Replace this with your real tests.
   test "Verify AJAX handlers for autocompletion are working" do
-    get :index, :format => :js
+    get :index, :format => :js, :search => 'plain'
     assert_response :success
-  end
+    assert_equal 3, assigns( :users ).size
+
+    get :index, :format => :js, :search => 'pu23456'
+    assert_response :success
+    assert_equal 1, assigns( :users ).size
+end
 end
