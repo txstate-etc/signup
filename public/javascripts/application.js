563dad6d500c6f33dfc8041d7b035de671371bf4
Sean McMains <seanmctex@txstate.edu>
2011-01-18 10:28:12 -0600

    Showing special accomodations text box only when needed.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function revealAccommodationsArea() {
+	Effect.BlindDown('special-accommodations-field', { duration: 0.5 } );
+}
+
+function hideAccommodationsArea() {
+	Effect.BlindUp('special-accommodations-field', { duration: 0.5 } );
+	$$('#special-accommodations-field textarea')[0].value = ""
+}
