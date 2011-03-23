2c2639c7bdae8fa31d3a6f119b01f72b70f4e2f2
Charles Jones <cj32@txstate.edu>
2011-03-23 13:42:27 -0500

    Tweaked styling to printed attendance sheet to reduce improve readability now that we are displaying email and department. bugid: 4242

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,11 @@
 module SessionsHelper
   def attendance_sheet_entry(user)
-    entry = user.name
-    entry << " (" if user.email || user.department
-    entry << user.email if user.email
-    entry << ", " if user.email && user.department
-    entry << user.department if user.department
-    entry << ")" if user.email || user.department
+    entry = simple_format(user.name)
+    line2 = ""
+    line2 << user.email if user.email
+    line2 << ", " if user.email && user.department
+    line2 << user.department if user.department
+    entry << simple_format(line2, :class => "line2") unless line2.blank?
     entry
   end
 end
