4cc9b44c7669e7dcbb86076cf890b9029fb2587e
Charles Jones <cj32@txstate.edu>
2011-03-23 13:04:24 -0500

    Added email to printed attendance list. bugid: 4242

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,11 @@
 module SessionsHelper
+  def attendance_sheet_entry(user)
+    entry = user.name
+    entry << " (" if user.email || user.department
+    entry << user.email if user.email
+    entry << ", " if user.email && user.department
+    entry << user.department if user.department
+    entry << ")" if user.email || user.department
+    entry
+  end
 end
