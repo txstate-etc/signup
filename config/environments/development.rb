fd79f6551d5f14718e03e198b79a977759b78b45
Charles Jones <cj32@txstate.edu>
2011-03-25 11:20:02 -0500

    Don't send emails in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,8 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-config.action_mailer.delivery_method = :smtp
+config.action_mailer.delivery_method = :test
+#config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address => 'smtp.txstate.edu',
   :domain => 'txstate.edu'
