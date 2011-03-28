60cf94a9983c03b07ef13306be7c593153d1ba43
Charles Jones <cj32@txstate.edu>
2011-03-28 11:41:11 -0500

    Updated unit test for attendance sheet. bugid: 4214

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -98,12 +98,13 @@ class SessionsControllerTest < ActionController::TestCase
     login_as( users( :instructor1 ) )
     get :attendance, {'id' => sessions( :gato_overbooked )}
     assert_response :success
+    assert_equal @response.content_type, 'application/pdf'
     assert_not_nil assigns(:session)
-    assert_not_nil assigns(:items)
-    assert_select 'ul' do |elements|
-      assert_equal elements.length, assigns(:page_count)
-      assert_select 'li', 2
-    end
+    # assert_not_nil assigns(:items)
+    # assert_select 'ul' do |elements|
+    #   assert_equal elements.length, assigns(:page_count)
+    #   assert_select 'li', 2
+    # end
     
   end
   
