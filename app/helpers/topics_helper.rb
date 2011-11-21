93cb5b7eeae1d3830d4047efd8db1681cde909df
Charles Jones <cj32@txstate.edu>
2011-11-21 17:49:45 -0600

    Show the next session time and seats remaining in topic list. bugid: 3593

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,2 +1,17 @@
 module TopicsHelper
+
+  def link_to_session(session)
+    session_info = "#{formatted_time_range(session.time, session.topic.minutes)} ("
+    session_info << " #{session.confirmed_reservations.size}"
+    if session.seats
+      session_info << " / #{session.seats}"
+    end 
+      session_info << " registered"
+    if session.waiting_list.size > 0 
+      session_info << ", #{session.waiting_list.size} waiting"
+    end
+    session_info << " )"
+    link_to session_info, session
+  end
+
 end
