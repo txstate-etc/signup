38f1be85db8c38946abc2876b6381c294d16e725
Charles Jones <cj32@txstate.edu>
2012-05-08 12:03:52 -0500

    Fix crash when displaying 404 page for missing session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     begin
       @session = Session.find( params[:id] )
     rescue ActiveRecord::RecordNotFound
-      render(:file => 'shared/404.erb', :status => 404, :layout => true) unless @session
+      render(:file => 'shared/404.erb', :status => 404, :layout => 'application') unless @session
       return
     end
         
@@ -33,7 +33,7 @@ class SessionsController < ApplicationController
       begin
         @session = Session.find( params[ :id ] )
       rescue ActiveRecord::RecordNotFound
-        render(:file => 'shared/404.erb', :status => 404, :layout => true) unless @session
+        render(:file => 'shared/404.erb', :status => 404, :layout => 'application') unless @session
         return
       end
       @page_title = @session.topic.name
