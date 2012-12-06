2b132d5c24614de3e221b663b30de177869efecd
Charles Jones <cj32@txstate.edu>
2012-12-06 03:24:03 -0600

    fixed test since user autocomplete is now behind authentication. bugid: 4143

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
   test "Verify AJAX handlers for autocompletion are working" do
-    get :index, :format => :js, :search => 'plain user'
+    login_as( users( :admin1 ) )
+    get :search, :format => :js, :search => 'plain user'
     assert_response :success
     assert_equal 4, assigns( :users ).size
 
-    get :index, :format => :js, :search => 'pu23456'
+    get :search, :format => :js, :search => 'pu23456'
     assert_response :success
     assert_equal 1, assigns( :users ).size
   end
