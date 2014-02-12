812848cb39f3300d3651100310af4d32e190c642
Charles Jones <cj32@txstate.edu>
2014-02-11 19:00:19 -0600

    Added Mark All Attended/Absent buttons to attendance page. bugid: 5478

--- public/javascripts/attendance.js
+++ public/javascripts/attendance.js
@@ -1,4 +1,5 @@
 Event.observe(document, 'dom:loaded', function () {
+	
 	$$('p.special-accommodations').each( function (item) {
 		item.observe('click', function(event) {
 			if (this.hasClassName('collapsed')) {
@@ -7,6 +8,16 @@ Event.observe(document, 'dom:loaded', function () {
 				this.addClassName('collapsed');
 			}
 			new Effect.Highlight(this, { startcolor: '#a56769', endcolor: '#ffffff', restorecolor: 'transparent' });
+		});
+	});
+
+	$$('.radio-all-wrap input').each( function (item) {
+		item.observe('change', function(event) {
+			$$('.attendance-radios input').each( function (radio) {
+				if (radio.value == item.value) {
+					radio.checked = true;					
+				}
+			});
 		});
 	});
 });
