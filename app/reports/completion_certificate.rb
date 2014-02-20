6168eceea0765bc6fe63cbf056b6ce5e95eb7861
Charles Jones <cj32@txstate.edu>
2014-02-19 18:21:29 -0600

    Redesigned certificate from Laura's input. bugid: 3525

--- app/reports/completion_certificate.rb
+++ app/reports/completion_certificate.rb
@@ -1,74 +1,70 @@
 class CompletionCertificate < Prawn::Document
   include ApplicationHelper
-  
-  FONT_SIZE = 18
-  LINE_HEIGHT = (FONT_SIZE * 1.3).ceil
-  LINE_SPACING = LINE_HEIGHT + 10
-  TOP = 455
 
   def initialize
     super(:page_layout => :landscape)
   end
 
   def to_pdf(reservation)
-    self.line_width = 3
-    self.join_style = :bevel
-    stroke_color '836c31'
-    stroke_bounds
 
-    bounding_box([5,535], :width => 710, :height =>530) do
+    # top border
+    image "#{Rails.root}/public/images/certificate/topbar.jpg", :width => 749, :height => 20, :position => :center, :vposition => -15
 
-      self.line_width = 1
-      self.join_style = :miter
-      stroke_color '501214'
-      stroke_bounds
-        
-      bounding_box([15,535], :width => 680, :height =>530) do
-        pad(25) do 
+    # header
+    image "#{Rails.root}/public/images/certificate/certificate.jpg", :width => 688, :height => 46, :position => :center, :vposition => 28
 
-          stroke_color '444444'
-          fill_color '836c31'
-          text 'Certificate of Completion', :mode => :fill_stroke, :size => 48, :style => :italic, :align => :center
+    # main body text
+    line reservation.user.name, :ypos => 425, :height => 48, :size => 48, :style => :italic, :font => 'Times-Roman'
+    line 'has successfully completed the course', :ypos => 350, :color => '75746D'
+    line reservation.session.topic.name, :ypos => 295, :height => 65, :size => 28, :style => :bold
+    line fmt_date_range(reservation.session), :ypos => 185, :color => '75746D'
+    line 'instructed by:', :ypos => 125, :color => '75746D'
+    line reservation.session.topic.department.name, :ypos => 100, :color => '75746D'
           
-          fill_color '000000'
-
-          static 'This is to certify that'
-          dynamic reservation.user.name
-          static 'has successfully completed the course'
-          dynamic reservation.session.topic.name, :height => 2*LINE_HEIGHT, :overflow => :shrink_to_fit
-          static 'on', :ydelta => 1.7*LINE_SPACING
-          dynamic reservation.session.last_time.strftime('%A, %B %e, %Y')
-          static 'taught by'
-          dynamic reservation.session.instructors.map {|i| i.name }.join(',')
-          dynamic reservation.session.topic.department.name, :ydelta => LINE_HEIGHT
-          dynamic I18n.t(:organization), :ydelta => LINE_HEIGHT
-          if (File.exists?("#{Rails.root}/public/images/cert-logo.jpg"))
-            image "#{Rails.root}/public/images/cert-logo.jpg", :position => :center, :vposition => :bottom
-          end
-        end
-      end
+    # organization logo
+    if (File.exists?("#{Rails.root}/public/images/certificate/logo.jpg"))
+      image "#{Rails.root}/public/images/certificate/logo.jpg", :width => 139, :height => 36, :position => :center, :vposition => 485
     end
-          
+
+    # bottom border
+    image "#{Rails.root}/public/images/certificate/bottombar.jpg", :width => 749, :height => 20, :position => :center, :vposition => 535
+
     render
   end
 
   protected
 
-  def static(str, opts={})
-    text_box str, text_opts(opts)
-  end
-
-  def dynamic(str, opts={})
-    text_box str, text_opts({:style => :bold}.merge(opts))
+  def line(str, opts={})
+    fill_color opts.delete(:color) || '000000'
+    font opts.delete(:font) || 'Helvetica'
+    topts = text_opts(opts)
+    text_box str, topts
+    # stroke_rectangle(topts[:at], topts[:width] || bounds.width, topts[:height])
   end
 
   def text_opts(opts={})
-    {:at => [0, ypos(opts)], :height => LINE_HEIGHT, :size => FONT_SIZE, :align => :center, :valign => :center}.merge(opts)
+    {:at => [(bounds.width - 600)/2, opts.delete(:ypos)], :width => 600, :height => 20, :size => 16, :align => :center, :valign => :center, :overflow => :shrink_to_fit, :min_font_size => 12}.merge(opts)
   end
 
-  def ypos(opts)
-    @ypos ||= TOP
-    @ypos -= opts.delete(:ydelta) || LINE_SPACING
+  def fmt_date_range(session)
+    if session.multiple_occurrences?
+      first = session.time.to_date
+      last = session.last_time.to_date
+      if first.year == last.year
+        if first.month == last.month
+          # same month
+          first.strftime('%B %-e') + '-' + last.strftime('%-e, %Y')
+        else
+          # diff month, same year
+          first.strftime('%B %-e') + '-' + last.strftime('%B %-e, %Y')
+        end
+      else
+        # diff year
+        first.strftime('%B %-e, %Y') + ' - ' + last.strftime('%B %-e, %Y')
+      end
+    else  
+      session.last_time.strftime('%B %-e, %Y')
+    end
   end
   
 end
