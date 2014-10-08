b6795c04fec407708a85cee562f434a2576b8c57
Charles Jones <cbj3@chuckbjones.com>
2014-10-08 15:01:00 -0500

    Highlight link to current page.

--- app/assets/javascripts/topics.js
+++ app/assets/javascripts/topics.js
@@ -14,3 +14,28 @@ $(function() {
     }
   });
 });
+
+
+// Highlight the sidebar link that matches the page we are on, if any.
+$(function() {
+  if (window.location.pathname == '/') {
+    $('#topic-list-link').addClass('selected');
+    $('#topic-list-nav a:first').addClass('selected');
+  } else {
+    var cur_url = window.location.href.split('#')[0];
+  
+    // special case: highlight "Available Topics" link on any related page:
+    // group by department, group by date, month at a glance
+    if (new RegExp("/topics($|/alpha|/grid|/by-site|/by-department)").test(cur_url)) {
+      $('#topic-list-link').addClass('selected');
+    }    
+    $('#topic-list-link, div#topic-list-nav a, div.navigation a, div.admin-tools a').each(function() {
+      if (this.href == cur_url) {
+        $(this).addClass('selected');
+      }
+    });
+    if (cur_url.match('/topics/grid')) {
+      $('a[href="/topics/grid"]').addClass('selected');
+    }
+  }  
+});
