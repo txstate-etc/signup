96658ed96b8469b06a44d21b41742b0ee585ae4c
Charles Jones <cj32@txstate.edu>
2014-10-18 15:19:25 -0500

    When parsing tag_list, the input may not be a string.

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -2,7 +2,7 @@ class DelimiterParser < ActsAsTaggableOn::GenericParser
   def parse
     ActsAsTaggableOn::TagList.new.tap do |tag_list|
       # FIXME: we used to split on white space. No More?
-      tag_list.add @tag_list.split(/,|;/).map(&:titleize)
+      tag_list.add @tag_list.to_s.split(/,|;/).map(&:titleize)
     end
   end
 end
