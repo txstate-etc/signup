b34064af451f2964516e8ae5d3366b054e26fd9d
Charles Jones <cbj3@chuckbjones.com>
2014-11-01 16:21:02 -0500

    Add migration in new version of acts_as_taggable_on

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141031204752) do
+ActiveRecord::Schema.define(version: 20141101211731) do
 
   create_table "auth_sessions", force: true do |t|
     t.string   "credentials", null: false
@@ -147,6 +147,7 @@ ActiveRecord::Schema.define(version: 20141031204752) do
   end
 
   add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
 
   create_table "tags", force: true do |t|
     t.string  "name"
