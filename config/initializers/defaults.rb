e0f069084e42f3f0aee7e151e62b4c8dd606c9bb
Charles Jones <cj32@txstate.edu>
2014-11-03 10:45:52 -0600

    Explicitly search for userid in People Search, because it is not used by default. It only worked by coincidence before because most people's email address contains their NetID.

--- config/initializers/defaults.rb
+++ config/initializers/defaults.rb
@@ -1,1 +1,1 @@
-DIRECTORY_URL_BASE = 'http://peoplesearch.txstate.edu/peoplesearch.pl?srchmode=search&query=##LOGIN##'
+DIRECTORY_URL_BASE = 'http://peoplesearch.txstate.edu/peoplesearch.pl?srchmode=search&query=userid%3D##LOGIN##'
