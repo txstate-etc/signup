61ceafa786536fec227c6d9d6709d905f6270c84
Charles Jones <cj32@txstate.edu>
2014-12-19 14:23:21 -0600

    Make sure image urls are generated using asset paths in emails. #6375

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -64,7 +64,7 @@ module TopicsHelper
 
     url = opts.delete(:url) || file_icon.icon_path
     url_proc = opts.delete(:url_proc)
-    url = url_proc.call(url) unless url_proc.nil? 
+    url = url_proc.call(image_path(url)) unless url_proc.nil? 
 
     opts = { :alt => t(:'file_icon.type', :type => (file_icon.type || filename)) }.merge opts
     image_tag(url, opts)
